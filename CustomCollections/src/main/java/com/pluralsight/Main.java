fcb5276bb2075783efd686ab8a462d1a060d022c
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-05-20 10:33:44 -0400

    Started Ex 1 pg 64-65 wb6

--- CustomCollections/src/main/java/com/pluralsight/Main.java
+++ CustomCollections/src/main/java/com/pluralsight/Main.java
@@ -1,17 +1,24 @@
 package com.pluralsight;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.pluralsight.collection.FixedList;
+
+import java.time.LocalDate;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        FixedList<Integer> numbers = new FixedList<>(3);
+        numbers.add(10);
+        numbers.add(3);
+        numbers.add(92);
+        numbers.add(43); // this line should fail
+        System.out.println("Number Size: " + numbers.getItems().size());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(numbers.getItems().size());
+        FixedList<LocalDate> dates = new FixedList<>(2);
+        dates.add(LocalDate.now());
+        dates.add(LocalDate.now());
+       // dates.add(15); // this line should fail
+        System.out.println("Time Size: " + numbers.getItems().size());
     }
 }
--- CustomCollections/src/main/java/com/pluralsight/collection/FixedList.java
+++ CustomCollections/src/main/java/com/pluralsight/collection/FixedList.java
@@ -1,4 +1,28 @@
 package com.pluralsight.collection;
 
-public class FixedList <T>{
+import java.util.ArrayList;
+import java.util.List;
+
+public class FixedList<T> {
+   private List<T> items;
+   private int maxSize;
+
+   //Constructor
+    public FixedList(int maxSize) {
+        this.maxSize = maxSize;
+        this.items = new ArrayList<>();
+    }
+
+    //add and see if size is less than the size
+    public void add(T item) {
+        if (items.size() < maxSize) {
+            items.add(item);
+        }
+    }
+
+    //getter of the list
+    public List<T> getItems() {
+        return new ArrayList<>(items);
+    }
 }
+
